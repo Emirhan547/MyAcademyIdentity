3ac6b227705502be449e961bb5f3258d41802d7c
Emirhan547 <emirhanhacioglu0@gmail.com>
2025-09-09 21:46:08 +0300

    Add project files.

---
+++ EmailApp/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using EmailApp.Entities;
+using EmailApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user is null)
+            {
+                ModelState.AddModelError("", "Bu email sistemde kayıtlı değil");
+                return View(model);
+            }
+            var result = await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", " email veya şifre hatalı");
+                return View(model);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
+       
---
+++ EmailApp/Validations/CustomErrorDescriber.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EmailApp.Validations
+{
+    public class CustomErrorDescriber : IdentityErrorDescriber
+    {
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresDigit),
+                Description = "Parola en az bir rakam içermelidir."
+            };
+        }
+
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresLower),
+                Description = "Parola en az bir küçük harf içermelidir."
+            };
+        }
+
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresUpper),
+                Description = "Parola en az bir büyük harf içermelidir."
+            };
+        }
+
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresNonAlphanumeric),
+                Description = "Parola en az bir özel karakter içermelidir."
+            };
+        }
+
+        public override IdentityError DuplicateUserName(string userName)
+        {
+            return new IdentityError
+            {
+                Code = nameof(DuplicateUserName),
+                Description = $"'{userName}' kullanıcı adı zaten alınmış."
+            };
+        }
+
+        public override IdentityError DuplicateEmail(string email)
+        {
+            return new IdentityError
+            {
+                Code = nameof(DuplicateEmail),
+                Description = $"'{email}' e-posta adresi zaten kullanılmaktadır."
+            };
+        }
+    }
+}
